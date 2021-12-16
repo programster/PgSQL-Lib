8fdedef0ccfdcbdfd5dfc4f324355c4770f1f09f
Programster <sdpagent@gmail.com>
2021-12-16 17:25:45 +0000

    adds exception for when trying to operate on no data provided

---
+++ src/exceptions/ExceptionNoData.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * An exception to throw if there is a connection error with the postgresql database.
+ */
+
+namespace Programster\PgsqlLib\Exceptions;
+
+
+use Exception;
+
+class ExceptionNoData extends Exception
+{
+
+}
